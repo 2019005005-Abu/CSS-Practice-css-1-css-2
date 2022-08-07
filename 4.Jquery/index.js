93eb15393d4dd3da1ad80289e9332636dd1642f7
Abu Al Shahriar Rifat <rifatdiu2686@gmail.com>
2022-08-07 17:08:20 +0600

    first commit

---
+++ 4.Jquery/index.js
@@ -0,0 +1,13 @@
+
+$("h1").text("Good Bye India")
+$("h1").text("I am a full stack web application Developement");
+$("#id_1").html("<b>You are  so beautiful</b>")
+$("#id_1").append("Goodbye");
+$("#id_1").prepend("This is a Prepend service");
+var myPara_1=$("<p></p>").text("This is Paragraph 0");
+$("#id_1").after(myPara_1);
+$("#id_1").before(myPara_1);
+var name=document.querySelector("a").getAttribute("href");
+$("a").r("href");
+$("a").attr("href","http://www.studywithanish.com")
+ 
